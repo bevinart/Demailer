538c80d070909e026f3c6c92fa8c4c0263c7277f
bevinart <ebpisarsky@gmail.com>
2024-05-03 03:55:11 +0000

    Added GmailAPI scope to SCOPES list to gain full access to Gmail Inbox

--- Demailer/__main__.py
+++ Demailer/__main__.py
@@ -3,7 +3,11 @@
 import demailer_gui as dgui
 import gmail_fetcher as gfetch
 
-SCOPES = []
+
+SCOPES = [
+    # SCOPE according to Google API: Read, compose, send, and permanently delete all your email from Gmail 
+    "https://mail.google.com/" 
+]
 
 if __name__ == "__main__":
     pass
