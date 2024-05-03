75ba846c9a9687ddcf83e1ef24ed083145d88699
bevinart <ebpisarsky@gmail.com>
2024-05-03 03:56:22 +0000

    Finished function 'get_user_token()' that returns a token for GoogleAPI once user logs in.

--- Demailer/gmail_fetcher.py
+++ Demailer/gmail_fetcher.py
@@ -1,6 +1,11 @@
 import os.path
+from pathlib import Path
 
-# NOTE: This class MUST have a token readily available in order to use this class. If you do not have a token, please refer to function 'get_user_token()'
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# NOTE: This class MUST have a token readily available in order to use this class. If you do not have a token, please refer to function 'get_user_token()' in class 'GoogleAPI'
 class GmailFetcher():
     def __init__(self, token):
         if self._is_token(token) == False:
@@ -14,6 +19,30 @@ class GmailFetcher():
     
     def _is_token(token):
         return True
+
+# This function will return a JSON variable
+def get_user_token(cred_json_path, scopes):
+    # Using pathlib in order to turn the string into a path that the system can read
+    cred_json_path = Path(cred_json_path)
     
-def get_user_token(client_id, client_secret, scopes):
-    pass
+    # token.json is used in order to prevent logging in everytime you run the application. It is made during this function, so if first time running, it will make one. 
+    token_json_path = Path("./token.json")
+    creds = None
+    
+    # This is used to grab any valid tokens from the JSON file if there is any
+    if os.path.exists(token_json_path):
+        creds = Credentials.from_authorized_user_file(token_json_path, scopes)
+    
+    # If there are no creds available that are valid, Refresh or Login
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(cred_json_path, scopes)
+            creds = flow.run_local_server(port=0)
+
+    # Save the creds for the next time the application is ran
+    with open(cred_json_path, "w") as cred_file:
+        cred_file.write(creds.to_json())
+
+    return creds
