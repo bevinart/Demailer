140a236ead950f3a869ce13a8ca996be8985fdd0
bevinart <ebpisarsky@gmail.com>
2024-05-08 03:54:19 +0000

    TEMP: __main__ is being used as a form of testing for the backend

--- Demailer/__main__.py
+++ Demailer/__main__.py
@@ -1,6 +1,8 @@
 # NOTE: This file will be the one that grabs the token from the Google API
 
+import os
 import demailer_gui as dgui
+import demailer_backend as dback
 import gmail_fetcher as gfetch
 
 
@@ -9,5 +11,12 @@ SCOPES = [
     "https://mail.google.com/" 
 ]
 
+# Right now, we are going to test the backend of everything before we make a GUI
 if __name__ == "__main__":
+    # Grab the token we need to access the Gmail API
+    cred_path = os.getcwd() + "/credentials.json"
+    creds = gfetch.get_user_token(cred_path, SCOPES)
+
+    gfetch.get_inbox(creds, 0, 0)
+
     pass
