25ea0c9c19c2bebd6225462768b73d6510d63359
bevinart <ebpisarsky@gmail.com>
2024-05-08 02:50:03 +0000

    Fixed outputting content to wrong JSON

--- Demailer/gmail_fetcher.py
+++ Demailer/gmail_fetcher.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 from pathlib import Path
 
 from google.oauth2.credentials import Credentials
@@ -8,17 +8,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 # NOTE: This class MUST have a token readily available in order to use this class. If you do not have a token, please refer to function 'get_user_token()' in class 'GoogleAPI'
 class GmailFetcher():
     def __init__(self, token):
-        if self._is_token(token) == False:
-            return (1, "Token is incorrect")
         self.token = token
     
     # This class will return a specified number of emails from the token's inbox, starting at the offset
     def get_inbox(self, num_of_emails, offset):
         pass
-    
-    
-    def _is_token(token):
-        return True
+
+
 
 # This function will return a JSON variable
 def get_user_token(cred_json_path, scopes):
@@ -26,7 +22,7 @@ def get_user_token(cred_json_path, scopes):
     cred_json_path = Path(cred_json_path)
     
     # token.json is used in order to prevent logging in everytime you run the application. It is made during this function, so if first time running, it will make one. 
-    token_json_path = Path("./token.json")
+    token_json_path = Path(os.getcwd() + "/token.json")
     creds = None
     
     # This is used to grab any valid tokens from the JSON file if there is any
@@ -42,7 +38,7 @@ def get_user_token(cred_json_path, scopes):
             creds = flow.run_local_server(port=0)
 
     # Save the creds for the next time the application is ran
-    with open(cred_json_path, "w") as cred_file:
-        cred_file.write(creds.to_json())
+    with open(token_json_path, "w") as token_file:
+        token_file.write(creds.to_json())
 
     return creds
