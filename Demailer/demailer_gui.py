8795b9530fa4dd5fa279f79ce133ed3bf93e8882
bevinart <ebpisarsky@gmail.com>
2024-05-09 20:08:02 +0000

    [WIP] Added placeholder functions for later

--- Demailer/demailer_gui.py
+++ Demailer/demailer_gui.py
@@ -1,2 +1,11 @@
 import demailer_backend as dback
 import tkinter as tk
+
+# THIS IS THE ENTRY POINT OF THE PROGRAM!!
+# This function will be used to represent to the user that the GUI is in fact loading, and that we are just waiting for confirmation from Google API.
+def login_window():
+    pass
+
+
+def _display_error():
+    pass
