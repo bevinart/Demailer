d4b975d4c6f3a552e4fc211afb3e94733a95b75d
bevinart <ebpisarsky@gmail.com>
2024-05-09 20:07:38 +0000

    __main__.py is now only responsible for passing responsibility to the frontend of Demailer

--- Demailer/__main__.py
+++ Demailer/__main__.py
@@ -1,17 +1,6 @@
-# NOTE: This file will be the one that grabs the token from the Google API
-
 import os
 import demailer_gui as dgui
-import demailer_backend as dback
 
-SCOPES = [
-    # SCOPE according to Google API: Read, compose, send, and permanently delete all your email from Gmail 
-    "https://mail.google.com/" 
-]
-
-# Right now, we are going to test the backend of everything before we make a GUI
+# '__main__.py' will be used to pass on responsibility to the 'demailer_gui.py'
 if __name__ == "__main__":
-    # Grab the token we need to access the Gmail API
-    cred_path = os.getcwd() + "/credentials.json"
-
-    pass
+    dgui.login_window()
