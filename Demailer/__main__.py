cacac4e73d4954376300a9f725797473eb3324fe
bevinart <ebpisarsky@gmail.com>
2024-05-09 16:05:30 +0000

    Removed content related to deleted 'gmail_fetcher.py' file

--- Demailer/__main__.py
+++ Demailer/__main__.py
@@ -3,8 +3,6 @@
 import os
 import demailer_gui as dgui
 import demailer_backend as dback
-import gmail_fetcher as gfetch
-
 
 SCOPES = [
     # SCOPE according to Google API: Read, compose, send, and permanently delete all your email from Gmail 
@@ -15,8 +13,5 @@ SCOPES = [
 if __name__ == "__main__":
     # Grab the token we need to access the Gmail API
     cred_path = os.getcwd() + "/credentials.json"
-    creds = gfetch.get_user_token(cred_path, SCOPES)
-
-    gfetch.get_inbox(creds, 0, 0)
 
     pass
